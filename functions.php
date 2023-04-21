5ac7d4ec501639f861ecfefb3f948699be75ba91
abbeytc27 <33941909@student.murdoch.edu.au>
2023-04-21 09:54:41 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,9 @@ function load_my_scripts() {
   wp_enqueue_script( 'boot1','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js', array('jquery'));
   wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js', array('jquery'));
   wp_enqueue_script( 'boot3','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js', array('jquery'));
- 
+  wp_enqueue_script( 'lottie-player', 'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js', array(), false, true );
+  wp_enqueue_script( 'lottie-interactive', 'https://unpkg.com/lottie-interactive@latest/dist/lottie-interactive.js', array(), false, true );
+
   wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
 
